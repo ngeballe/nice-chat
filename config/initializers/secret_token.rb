5ea76eeeeb2325cf5aba6bcc1486c3da05578b3d
Nick Geballe <ngeballe@gmail.com>
2013-10-08 17:50:26 -0400

    First commit and README update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NiceChat::Application.config.secret_token = 'd7760739ddc09775e5a25fe6eabc616985db0bd6b2643392f52b77d3edb7f18b2a6687e5f8f195a1ae831e31d0d9a21cc2bebf4c7bd518c2be0afb83993d28b5'
